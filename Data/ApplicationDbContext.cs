11620bc455010b0201b54de912fe69658180c91b
蛉黎 <xeamrx@gmail.com>
2025-09-12 16:17:56 +0800

    廠商註冊完成

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Cat_Paw_Footprint.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,18 @@ namespace Cat_Paw_Footprint.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
-    }
+	    }
+		public DbSet<Vendors> Vendors { get; set; } = null!;
+		public DbSet<Customers> Customers { get; set; } = null!;
+		public DbSet<Models.CustomerProfile> CustomerProfiles { get; set; }
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<Vendors>()
+				.HasOne(v => v.User)
+				.WithOne() // IdentityUser 不需要反向導航
+				.HasForeignKey<Vendors>(v => v.UserId);
+		}
+	}
 }
